3684a7fbfbc42467e31eaddcbf303c868a0c4466
CaiCode <m15267946832@163.com>
2017-08-07 23:31:30 +0800

    ADD metalClass

--- Python_L.py
+++ Python_L.py
@@ -534,6 +534,8 @@ print(L)
 #ORM框架 对象—关系映射
 
 #Field Class
+
+'''
 class Field(oject):
 	def __init__(self, name, column_type):
 		self.name = name
@@ -551,3 +553,51 @@ class StringField(Field):
 class IntegerField(Field):
 	def __init__(self,name):
 		super(IntegerField,self).__init__(name,'bigint')
+
+class ModelMetaclass(type):
+
+	def __new__(cls, name, bases, attrs):
+		if name = 'Model':
+			return type.__new__(cls,name,bases,attrs)
+		print('Found model: %s' %name)
+		mappings = dict()
+		for k,v in attrs.items():
+			if isinstance(v, Field):
+				print('Found mapping: %s ==> %s' %(k,v))
+				mappings[k] = v
+		for k in mappings.keys():
+			attrs.pop(k)
+		attrs['__mappings__'] = mappings
+		attrs['__table__'] = name
+		return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass = ModelMetaclass):
+
+	def __init__(self, **kw):
+		super(Model,self).__init__(**kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'"%key)
+
+	def __setattr__(self,key ,value):
+		self[key] = value
+
+	def save(self):
+		fields = []
+		params = []
+		args = []
+		for k,v in self.__mappings__.items():
+			fields.append(v.name)
+			params.append('?')
+			args.append(getattr(self,k,None))
+		sql = 'insert into %s (%s) values (%s)' %(self.__table__,','.join(fields),','.join(params))
+		print('SQL:%s' %sql)
+		print('ARGS:%s' %str(args))
+'''
+
+
+
+
